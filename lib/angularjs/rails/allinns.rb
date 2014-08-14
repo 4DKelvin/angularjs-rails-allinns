1d2c562e3806d1da256cecabb7579a5368530129
Kelvin <kelvin@bestapp.us>
2014-08-15 05:36:17 +0800

    create angularjs gem

---
+++ lib/angularjs/rails/allinns.rb
@@ -0,0 +1,9 @@
+require "angularjs/rails/allinns/version"
+
+module Angularjs
+  module Rails
+    module Allinns
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/angularjs/rails/allinns/version.rb
@@ -0,0 +1,7 @@
+module Angularjs
+  module Rails
+    module Allinns
+      VERSION = "0.0.1"
+    end
+  end
+end
